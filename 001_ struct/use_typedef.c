c6e50e28e1753f6bf45a926cf7bd5d94db52c67d
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-02-28 14:26:40 +0000

    Correct typos

--- 001_ struct/use_typedef.c
+++ 001_ struct/use_typedef.c
@@ -9,7 +9,7 @@ typedef struct {
 int main()
 {
 	soldier s1 = {"23-76004321", "Cho Yoonsik", 3};
-	soldier s2 = {"22-76001234", "Kin Daeyeon", 1};
+	soldier s2 = {"22-76001234", "Kim Daeyeon", 1};
 	soldier s3 = {"22-76001111", "Sung Minjae", 2};
 	
 	printf("--Serial Number of %s--\n %s\n", s1.name, s1.serialNum );
