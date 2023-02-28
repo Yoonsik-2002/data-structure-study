928cf325fd52dd2809093761199f8f6e56cb5f83
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-02-28 14:06:30 +0000

    basic_struct.c

--- 001_ struct/basic_struct.c
+++ 001_ struct/basic_struct.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+struct car {
+	char manufacturer[10];
+	char name[10];
+	int price;
+};
+
 int main()
 {
-	printf("Hello World!");
+	struct car car1 = {"BMW", "5 series", 67600000};
+	struct car car2 = {"KIA", "K 7", 39690000};
+	struct car car3 = {"Mclaren", "675lt", 440000000};
+	
+	printf("--Information of %s--\n Manufacturer : %s \n Name : %s \n Price : %d\n", car3.name, car3.manufacturer, car3.name, car3.price);
+	printf("------------------------\n");
 }
