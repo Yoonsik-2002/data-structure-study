7cdc56229c4f1a11a7389f8fd8dd4eb9c5693858
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-03-03 13:42:08 +0000

    struct - accessing data inside a struct

---
+++ 001_ struct/struct_access.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+typedef struct{
+	int ID;
+	char name[20];
+	int age;
+	float height;
+	float weight;
+}personal_info;
+
+int main()
+{
+	personal_info my_info; // 구조체 변수 my_info 선언
+	
+	my_info.ID = 202343432;
+	strcpy(my_info.name, "조윤식");
+	my_info.age = 22;
+	my_info.height = 172.6;
+	my_info.weight = 63.4;
+	
+	printf("Information of %s\n ID : %d \n Name : %s \n Age : %d \n Height : %.1f \n Weight : %.1f\n", my_info.ID, 
+		   my_info.name, my_info.age, my_info.height, my_info.weight);
+}
