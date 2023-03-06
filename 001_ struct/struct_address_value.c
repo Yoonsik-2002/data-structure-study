e53cc86966c828d783d5e7aea3ece3ebb89bdc9e
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-03-06 10:23:55 +0000

    struct - address value of first member of struct

---
+++ 001_ struct/struct_address_value.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef struct {
+	int ID;
+	char Pass_Word[20];
+}member;
+
+int main()
+{
+	member example_member = {20211059, "asdfe1203"};
+	member * pptr = &example_member;
+	
+	printf("구조체 변수 example_member의 주소값 : %p\n", pptr); // 구조체 포인터 변수 pptr이 가리키고 있는 구조체 변수 example_member의 주소값
+	printf("구조체 변수 example_member의 첫 번째  멤버의 주소값 : %p\n", &pptr->ID); // 구조체 포인터 변수 pptr이 가리키고 있는 구조체 변수 example_member의 첫 번째 멤버 ID의 주소값
+	
+}
