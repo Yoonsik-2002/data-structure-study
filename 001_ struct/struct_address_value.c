c37b803b6e166833ab8e7f666c7afe55b5bcede0
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-03-06 10:45:29 +0000

    modify struct_address_value.c

--- 001_ struct/struct_address_value.c
+++ 001_ struct/struct_address_value.c
@@ -12,5 +12,4 @@ int main()
 	
 	printf("구조체 변수 example_member의 주소값 : %p\n", pptr); // 구조체 포인터 변수 pptr이 가리키고 있는 구조체 변수 example_member의 주소값
 	printf("구조체 변수 example_member의 첫 번째  멤버의 주소값 : %p\n", &pptr->ID); // 구조체 포인터 변수 pptr이 가리키고 있는 구조체 변수 example_member의 첫 번째 멤버 ID의 주소값
-	
 }
