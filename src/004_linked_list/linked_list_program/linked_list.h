ef62eb9d594defff41658e78445c11be0a025d22
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-07-30 07:04:32 +0000

    linked list - add test_linked_list.c file

--- src/004_linked_list/linked_list_program/linked_list.h
+++ src/004_linked_list/linked_list_program/linked_list.h
@@ -21,3 +21,10 @@ typedef struct tagNode {
 Node* SSL_CreateNode(ElementType NewData);
 void SSL_DestroyNode(Node* Node);
 Node* SSL_AppendNode(Node** Head, Node* NewNode);
+void SSL_InsertNodeAfter(Node* Current, Node* NewNode);
+void SSL_InsertNewHead(Node** Head, Node* NewNode);
+void SSL_RemoveNode(Node** Head, Node* Remove);
+Node* SSL_GetNodeAt(Node* Head, int Location);
+int SSL_GetNodeCount(Node* Head);
+
+#endif
