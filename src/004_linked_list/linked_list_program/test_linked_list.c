d03540d03dc0061d1dc842ebc1d105189f1ac852
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-07-30 11:15:15 +0000

    linked_list - modify test_linked_list.c

--- src/004_linked_list/linked_list_program/test_linked_list.c
+++ src/004_linked_list/linked_list_program/test_linked_list.c
@@ -30,4 +30,21 @@ void main()
 		
 		printf("List[%d] : %d \n", i, Current -> Data);
 	}
+	
+	// 리스트의 세 번째 노드 뒤에 새로운 노드 삽입
+	printf("\n Inserting 3000 After [3] ... \n\n");
+	
+	Current  = SSL_GetNodeAt(List, 3);
+	NewNode = SSL_CreateNode(3000);
+	
+	SSL_InsertNodeAfter(Current, NewNode);
+	
+	// 리스트 출력
+	Count = SSL_GetNodeCount(List);
+	
+	for (i = 1; i <= Count; i++) {
+		Current = SSL_GetNodeAt(List, i);
+		
+		printf("List[%d] : %d \n", i, Current -> Data);
+	}
 }
