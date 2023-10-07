f491f92c1673e5f1818a9ca42c0c5421bde2079b
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-10-07 07:24:52 +0000

    sequential list - ListMain.c

--- src/004_linked_list/01_sequential_list/sequential_list_program/ListMain.c
+++ src/004_linked_list/01_sequential_list/sequential_list_program/ListMain.c
@@ -4,9 +4,9 @@
 int main(void) 
 {
 	// ArrayList의 생성 및 초기화
-	List list; 
+	List list;  // 리스트의 생성
 	int data;
-	ListInit(&list);
+	ListInit(&list);  // 리스트의 초기화 - 그저 데이터만 담기는 것이 아니라, 해당 데이터를 효율적으로 저장 및 참조하는데 쓰이는 데이터까지 초기환
 	
 	// 5개의 데이터를 저장
 	LInsert(&list, 11); LInsert(&list, 11);
@@ -16,11 +16,11 @@ int main(void)
 	// 저장된 데이터의 전체 출력
 	printf("현재 데이터의 수 : %d \n", LCount(&list));
 	
-	if(LFirst(&list, &data)) // 첫 번째 데이터 조회
+	if(LFirst(&list, &data))  // 첫 번째 데이터 조회
 	{
 		printf("%d", data);
 		
-		while(LNext(&list, &data)) // 두 번째 이후의 데이터 조회
+		while(LNext(&list, &data))  // 두 번째 이후의 데이터 조회
 			printf("%d ", data);
 	}
 	printf("\n\n");
