1f59585c55d677d529c3e1bad74d931f6ef45614
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-10-28 10:41:54 +0000

    point list program - Point.c

--- src/004_linked_list/01_sequential_list/point_list_program/ArrayList.h
+++ src/004_linked_list/01_sequential_list/point_list_program/ArrayList.h
@@ -1,5 +1,7 @@
 #ifndef __ARRAYLIST_H__  // 헤더 구분자의 명명 규칙 - 헤더파일의 파일명의 모든 영문자는 대문자로 바꾸고, '.' 은 '_' 로 바꾼다. 파일 이름의 앞, 뒤에 두개의 언더바 '__'를 붙인다. 
 #define __ARRAYLIST_H__ 
+#include "Point.h"
+
 /* 헤더파일의 중복삽입 문제를 방지해주는 header gaurd.
 #if(#ifndef, #ifdef)문 뒤에는 만드시 #endif문이 등장해야 하며, 이 두 지시자 사이에 존재하는 코드는
 조건(조건이 되는 매크로가 정의되어 있느냐 아니냐)에 따라 삽입 및 삭제가 이루어진다.*/
@@ -8,7 +10,7 @@
 #define FALSE	0  // 거짓을 표현하기 위한 메크로 정의
 
 #define LIST_LEN	100  // 구현하고자 하는 배열기반의 리스트의 배열의 크기
-typedef int LData;  // LData에 대한 typedef 선언
+typedef Point LData;  // LData에 대한 typedef 선언
 
 typedef struct __ArrayList {
 	LData arr[LIST_LEN];  // 리스트의 저장소인 배열
---
+++ src/004_linked_list/01_sequential_list/point_list_program/PointListMain.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Point.h"
+#include "ArrayList.h"
+
+int main(void) {
+	List list;  // list 구조체 변수 생성
+	Point compPos;
+	Point * ppos; // Point 타입의 구조체를 가리키는 구조체 포인터 변수 ppos
+	
+	ListInit(*list); // list 구조체의 멤버들의 값을 초기값으로 초기화
+	
+	// 1) 4개의 데이터 저장
+	ppos = (Point*)malloc(sizeof(Point)); 
+	/* 
+	Point 구조체 만큼의 크기를 가진 메모리 공간을 자유 메모리(Heap)에 할당. 
+	해당 메모리 공간을 이루는 첫 번째 바이트의 주소값을 반환하여 Point타입의 구조체 포인터 변수
+	ppos에 저장
+	*/
+	SetPointPos(ppos, 2, 1); // heap에 생성된 첫 번째 구조체의 멤버 xpos, ypos를 각각 2, 1로 초기화
+	LInsert(&list, ppos); // 해당 구조체의 주소값(ppos)을 list의 Point타입(LData) 배열형태의 저장공간 arr에 저장.
+	
+	ppos = (Point*)malloc(sizeof(Point));
+	SetPointPos(ppos, 2, 2);
+	LInsert(&list, ppos);
+	
+	ppos = (Point*)malloc(sizeof(Point));
+	SetPointPos(ppos, 3, 1);
+	LInsert(&list, ppos);
+	
+	ppos = (Point*)malloc(sizeof(Point));
+	SetPointPos(ppos, 3, 3);
+	LInsert(&list, ppos);
+	
+	// 2) 저장된 데이터의 출력
+	printf("현재 데이터의 수 : %d\n", LCount(&list));
+	
+	if(LFirst(&list, &ppos)) 
+	{
+		ShowPointPos(ppos); // 구조체 포인터 변수 ppos가 가리키고 있는 첫 번째 데이터를 출력해줌
+		
+		while(LNext(&jlist, &ppos)) 
+			ShowPointPos(ppos); // 두 번째로 저장되어 있는 데이터를 하나하나씩 넘어가면서 출력해줌 탐색 -> 반환(*pdata(ppos) = plist -> arr[plist -> curPosittion])
+	}
+	printf("\n");
+}
