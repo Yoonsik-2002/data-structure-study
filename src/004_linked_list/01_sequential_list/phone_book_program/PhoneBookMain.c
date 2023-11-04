8986244f500ec7be3620a9b72e9127e6c551007b
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-11-04 14:26:56 +0000

    phone book program - PhoneBookMain.c

---
+++ src/004_linked_list/01_sequential_list/phone_book_program/PhoneBookMain.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "NameCard.h"
+#include "ArrayList.h"
+
+void main() {
+	List list;
+	NameCard * pcard;
+	char tempArr[PHONE_LEN];
+	
+	ListInit(&list);
+	
+	// 기능 1) 총 3명의 전화번호 정보를 앞서 구현한 리스트에 저장
+	pcard = MakeNameCard("조윤식", "010-2342-2342");
+	LInsert(&list, pcard);
+	
+	pcard = MakeNameCard("김소정", "010-2342-2222");
+	LInsert(&list, pcard);
+	
+	pcard = MakeNameCard("장준혁", "010-2343-3333");
+	LInsert(&list, pcard);
+	
+	// 기능 2) 특정 이름을 대상으로 탐색을 진행하여 그 사람의 정보 출력(김소정)
+	if(LFirst(&list, &pcard))
+	{
+		if(!NameCompare(pcard, "김소정"))
+		{
+			ShowNameCardInfo(pcard);
+		}
+		else 
+		{
+			while(LNext(&list, &pcard)) 
+			{
+				if(!NameCompare(pcard, "김소정")) 
+				{
+					ShowNameCardInfo(pcard);
+					break;
+				}
+			}
+		}
+	}
+	
+	// 기능 3) 특정 이름을 대상으로 탐색을 진행하여 그 사람의 전화번호를 변경
+	if(LFirst(&list, &pcard))
+	{
+		if(!NameCompare(pcard, "장준혁"))
+		{
+			printf("enter the phone number : ");
+			scanf("%s", tempArr);
+			
+			ChangePhoneNumber(pcard, tempArr);
+		}
+		else
+		{
+			while(LNext(&list, &pcard)) 
+			{
+				if(!NameCompare(pcard, "장준혁"))
+				{
+					pritnf("enter the phone number : ");
+					scanf("%s", tempArr);
+					
+					ChangePhoneNumber(pcard, tempArr);
+				}
+			}
+		}
+	}
+}
