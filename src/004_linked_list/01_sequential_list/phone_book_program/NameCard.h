fbe894360f5c38f55a4cbd0e56d37b56d179a638
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-11-05 10:46:11 +0000

    phone book program - NameCard.h

--- src/004_linked_list/01_sequential_list/phone_book_program/NameCard.h
+++ src/004_linked_list/01_sequential_list/phone_book_program/NameCard.h
@@ -4,14 +4,14 @@
 #define NAME_LEN	30
 #define PHONE_LEN	30
 
-typedef struct __namecard
+typedef struct __namecard 
 {
 	char name[NAME_LEN];
 	char phone[PHONE_LEN];
 } NameCard;
 
 
-// NameCard 구조체 변수의 동적 할당 및 초기화 후 주소값 반환
+// NameCard 구조체 변수의 동적 할당 및 멤버(name, phone)초기화 후 주소값 반환
 NameCard * MakeNameCard(char * name, char * phone);
 
 // NameCard 구초제 변수의 정보 출력
