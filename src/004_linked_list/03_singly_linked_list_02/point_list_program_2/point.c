c49a3e90c931a5dd7ef25a6b7758b49d6b94cece
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-12-04 14:11:20 +0000

    point_list_program_2 - point.c

--- src/004_linked_list/03_singly_linked_list_02/point_list_program_2/point.c
+++ src/004_linked_list/03_singly_linked_list_02/point_list_program_2/point.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include "point.h"
 
+/* Point 구조체 변수의 멤버 xpos, ypos값을 초기화 */
 void SetPointPos(Point * ppos, int xpos, int ypos) 
 {
 	ppos -> xpos = xpos;
 	ppos -> ypos = ypos;
 }
 
+/* 인자로 전달받은 Point 구조체 변수의 멤버 xpos, ypos값을 출력 */
 void ShowPointPos(Point * ppos) 
 {
 	printf("[%d, %d]\n", ppos -> xpos, ppos -> ypos);
 }
 
+/* Point 구조체 변수의 xpos, ypos값을 서로 비교 */
 int PointComp(Point * pos1, Point * pos2) 
 {
 	if(pos1 -> xpos == pos2 -> xpos && pos1 -> ypos == pos2 -> ypos) 
