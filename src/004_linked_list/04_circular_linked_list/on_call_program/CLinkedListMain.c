86cedb596cad7376c815e97efe507879adbba278
Yoonsik-2002 <yoonsikp119105@gmail.com>
2024-02-11 14:46:15 +0000

    on-call program - CLinkedListMain.c

---
+++ src/004_linked_list/04_circular_linked_list/on_call_program/CLinkedListMain.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Employee.h"
+#include "CLinkedList.h"
+
+Employee* whosNightDuty(List * pList, char * name, int day);
+void showEmployeeInfo(Employee * pEmp);
+
+void main() {
+	
+	
+	/* 원형 연결 리스트의 생성 및 초기화 */
+	List, list;
+	ListInit(&list);
+	
+	/* 원형 연결 리스트에 4개의 사원 데이터 저장 */
+	
+}
