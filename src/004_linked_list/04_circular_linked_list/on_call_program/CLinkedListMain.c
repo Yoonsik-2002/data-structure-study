d3bb7d191eeb67b1c84ff05bd178216af48cea4c
Yoonsik-2002 <yoonsikp119105@gmail.com>
2024-02-15 13:10:43 +0000

    on-call program - CLinkedListMain.c

--- src/004_linked_list/04_circular_linked_list/on_call_program/CLinkedListMain.c
+++ src/004_linked_list/04_circular_linked_list/on_call_program/CLinkedListMain.c
@@ -53,30 +53,35 @@ void main() {
 			}
 		}
 	}
-}	
+}
 
 Employee* whosNightDuty(List * pList, char * name, int day) {
 	int i;
-	Data * rat;
+	Data * ret; 
 	
-	LFirst(&list, &rat); // 첫 번째 노드의 데이터(구조체 형태)를 구조체 포인터 변수 rat이 가리킴 
+	/* 매개변수로 전달받은 사원 탐색 */
+	LFirst(&list, &ret);
 	
-	/* 그냥 rat을 전달하는 경우 - 구조체 포인터 변수 rat이 저장하고 있는 값이 함수의 매개변수에 복사되어 전달된다.
-	해당 매개변수는 단지, rat이 저장하고 있는 값을 복사하여 저장하고 있는 별도의 구조체 포인터 변수이며, 해당 매개변수의 값을 변경하더라도
-	함수 외부에 있는 rat에게는 아무런 영향을 미치지 못한다. 또, 함수 종료 시, 소멸된다.
-	
-	-> 위 코드와 같이 함수 외부에 있는 구조체 포인터 변수 rat의 주소값을 매개변수에 전달받아 사용하면, rat의 값을 함수 내에서 변경 가능하다.
-	
-	   LFirst(&list, &rat); 구조체 포인터 변수 rat의 주소값이 매개변수에 복사되어 전달됨. CLL의 첫 번째 노드의 데이터를 rat의 주소값을 
-	   이용하여 가리키게 함. 함수가 종료되어도 이는 외부에 존재하는 rat에 그대로 적용되어 있음. */
-	
-	if(srtcmp(rat -> name, name) != 0) { // 첫 번째 노드의 데이터로 저장되어 있는 이름이 찾고자 하는 이름과 같지 않은 경우
+	if(strcmp(rat -> name, name) != 0) {
 		for(i = 0; i < LCount(&list)-1; i++) {
-			LNext(&list, &rat);
-			if(strcmp(rat -> name, name == 0)
-			   break;
+			LNext(&list, &ret);
+			
+			if(strcmp(ret -> name, name) == 0)
+				break;
 		}
-	}   
+		// if(ret == NULL) {
+		// 	return NULL;
+		// }
+		
+		if(i >= LCount(&list)-1) {
+			return NULL;
+		}
+	}
+	
+	/* 탐색한 사원으로 부터, day일 후, 당직 근무에 투입되는 사원을 탐색 */
+	for(i = 0; i < day; i++) {
+		LNext(&list, &ret);
+	}
+	
+	return ret;
 }
-
-
