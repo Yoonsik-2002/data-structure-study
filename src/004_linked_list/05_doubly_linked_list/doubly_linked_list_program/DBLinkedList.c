dc19bc623196e3aba718076e097d0721fbba0b74
Yoonsik-2002 <yoonsikp119105@gmail.com>
2024-03-03 10:59:11 +0000

    doubly_linked_list_program - DBLinkedList.c

---
+++ src/004_linked_list/05_doubly_linked_list/doubly_linked_list_program/DBLinkedList.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "DBLinkedList.h"
+
+/* 양방향 연결 리스트의 초기화 - ListInit */
+void ListInit(List * pList) {
+	pList -> head = NULL;
+	pList -> numOfData = 0;
+}
+
+/* 노드 삽입 - LInsert */
+void LInsert(List * pList, Data data) {
+	Node * newNode = (Node *)malloc(sizeof(Node));
+	newNode -> data = data;
+	
+	if(pList -> head != NULL) {
+		pList -> head -> prev = newNode;
+	}
+	
+	newNode -> next = head;
+	pList -> head = newNode;
+	(pList -> numOfData)++
+}
+
+/* 첫 번째 노드를 탐색 - LFirst */
+int LFirst(List -> pList, Data * pData) {
+	if(pLIst -> head == NULL) {
+		return FALSE;
+	}
+	
+	pList -> cur = pList -> head;
+	*pData = pList -> cur -> data;
+	
+	return TRUE;
+}
+
+/* 두 번쨰 노드를 탐색 - LNext */
+int LNext(List -> pList, Dat * pData) {
+	if(pList -> cur -> next == NULL) {
+		return FALSE;
+	}
+	
+	pList -> cur = pList -> cur -> next;
+	*pData = pList -> cur -> data;
+	
+	return TRUE;
+}
