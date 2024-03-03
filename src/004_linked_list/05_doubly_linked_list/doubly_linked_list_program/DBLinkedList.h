4a5f63e6e5081446d94fbdf09fdd4003307b1096
Yoonsik-2002 <yoonsikp119105@gmail.com>
2024-03-03 09:45:23 +0000

    doubly_linked_list_program - DBLinkedList.h

---
+++ src/004_linked_list/05_doubly_linked_list/doubly_linked_list_program/DBLinkedList.h
@@ -0,0 +1,30 @@
+#ifndef __DB_LINKEDLIST_H__
+#define __DB_LINKEDLIST_H__
+
+#define TRUE 	1
+#define	FALSE 	0
+
+typedef int Data;
+
+/* 양방향 연결 리스트의 노드를 표현하는 구조체 */
+typedef struct _node {
+	struct _node * next;
+	struct _node * prev;
+	Data data;
+} Node;
+
+/* 양방향 연결 리스트를 표현하는 구조체 */
+typedef struct _dbLinkedList {
+	struct _dbLinkedList * head;
+	struct _dbLinkedList * cur;
+	int numOfData;
+} DBLinkedList;
+
+typedef DBLinkedList List;
+
+/* 프로그램에 사용되는 함수들의 선언 */
+void ListInit(List * pList);
+void LInsert(List * pList, Data data);
+int LFirst(List * pList, Data * pData);
+int LNext(List * pLIst, Data * pData);
+
