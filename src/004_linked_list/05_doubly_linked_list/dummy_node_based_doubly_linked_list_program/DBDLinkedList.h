bc6a076b27149d7cb0a91916baf445ecfe549440
Yoonsik-2002 <yoonsikp119105@gmail.com>
2024-03-05 06:36:40 +0000

    dummy_node_based_linked_list_program - DBDLinkedList.h

---
+++ src/004_linked_list/05_doubly_linked_list/dummy_node_based_doubly_linked_list_program/DBDLinkedList.h
@@ -0,0 +1,36 @@
+#ifndef __DBDLinkedList_H__
+#define __DBDLinkedList_H__
+
+#define True 	1
+#define FALSE	0
+
+typedef int Data;
+
+/* 양방향 연결 리스트의 노드를 나타내는 구조체 */
+typedef struct _node {
+	struct _node * next;
+	struct _node * prev;
+	Data data;
+} Node;
+
+/* 양방향 연결 리스트를 나타내는 구조체 */
+typedef struct _DBDLinkedList {
+	struct _DBDLinkedList * head;
+	struct _DBDLinkedList * tail;
+	struct _DBDLinkedList * cur;
+	
+	int numOfData;
+} DBDLinkedList;
+
+typedef DBDLinkedList List;
+
+void ListInit(List * pList);
+void LInseret(List * pLIst, Data data);
+
+int LFirst(List * pList, Data * pData);
+int LNext(List * pLIst, Data * pData);
+
+Data LRemove(List * pList);
+int LCount(List * pList);
+
+#endif
