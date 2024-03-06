3bc352c555c45c21b051b25fd770de384a99d687
Yoonsik-2002 <yoonsikp119105@gmail.com>
2024-03-06 13:33:47 +0000

    dummy_node_based_doubly_linked_list_program - DBDLinkedListMain.c

--- src/004_linked_list/05_doubly_linked_list/dummy_node_based_doubly_linked_list_program/DBDLinkedListMain.c
+++ src/004_linked_list/05_doubly_linked_list/dummy_node_based_doubly_linked_list_program/DBDLinkedListMain.c
@@ -17,7 +17,9 @@ void main() {
 	LInsert(&list, 7); LInsert(&list, 8);
 	
 	/* 리스트에 저장된 데이터 출력 - 정방향, 역방향 */
-	if(LFirst(&list)) {
+	if(LFirst(&list, &data)) {
+		printf("%d ", data);
+		
 		/* 정방향으로 데이터 출력 */
 		while(LNext(&list, &data)) {
 			printf("%d ", data);
@@ -30,7 +32,10 @@ void main() {
 	}
 	
 	/* 짝수 데이터를 지닌 노드를 연결 리스트에서 삭제 */
-	if(LFirst(&list)) {
+	if(LFirst(&list, &data)) {
+		if(data % 2 == 0) 
+			LRemove(&list);
+		
 		while(LNext(&list, &data)) {
 			if(data % 2 == 0) 
 				LRemove(&list);
@@ -38,7 +43,9 @@ void main() {
 	}
 	
 	/* 데이터 삭제 후, 리스트의 남아있는 데이터를 출력 */
-	if(LFirst(&list)) {
+	if(LFirst(&list, &data)) {
+		printf("%d ", data);
+		
 		while(LNext(&list, &data)) {
 			printf("%d ", data);
 		}
